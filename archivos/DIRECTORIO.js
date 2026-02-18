9cbceb4634d46a9df4d357348a63fc894be8c0d8
josefrlnc01 <josefrlnc01@gmail.com>
2026-02-18 13:42:27 +0100

    trabajando con sistema de archivos de node

---
+++ archivos/DIRECTORIO.js
@@ -0,0 +1,18 @@
+export const personas = [
+    {
+        dni : "432424344",
+        name : "Jose"
+    },
+    {
+        dni : "43242433434",
+        name : "Marta"
+    },
+    {
+        dni : "432424367",
+        name : "Fran"
+    },
+    {
+        dni : "4324243890",
+        name : "Javier"
+    }
+]
--- archivos/script.js
+++ archivos/script.js
@@ -1,20 +1,28 @@
 import fs from "node:fs"
-
+import path from "node:path"
+import { personas } from "./DIRECTORIO.js"
+import { json } from "node:stream/consumers"
 function crearArchivo(nombre, edad, profesion){
-    const filePath = "agenda.json"
+    const rutaArchivo = "agenda.json"
 
-    if (!fs.existsSync(filePath)) {
-        throw new Error("El archivo aún no fue creado")
+    if (!fs.existsSync(rutaArchivo)) {
+        fs.writeFileSync(rutaArchivo, JSON.stringify([]))
+        console.log("Archivo creado")
     }
-    const archivo = fs.readFileSync(filePath, "utf-8")
+    const archivo = fs.readFileSync(rutaArchivo, "utf-8")
     const archivoParseado = JSON.parse(archivo)
     
     const existe = archivoParseado.some(val => 
         val.nombre === nombre && val.edad === edad && val.profesion === profesion
     )
+    const usuarioExistente = archivoParseado.filter(user => 
+        user.nombre === nombre && user.edad === edad && user.profesion === profesion
+    )
     
     if (existe) {
-        console.log("Este registro ya existe")
+        usuarioExistente.forEach(user => {
+            console.log(`El usuario ${user.nombre} con profesión ${user.profesion} y edad ${user.edad} ya existe`)
+        })
         return
     }
 
@@ -24,11 +32,169 @@ function crearArchivo(nombre, edad, profesion){
         profesion,
     }
     archivoParseado.push(persona)
-    fs.writeFileSync(filePath, JSON.stringify(archivoParseado, null, 2))
+    fs.writeFileSync(rutaArchivo, JSON.stringify(archivoParseado, null, 2))
 
 }
 
-crearArchivo("raul", 24, "backend")
+
+
+function leerArchivo(rutaArchivo) {
+    if (!fs.existsSync(rutaArchivo)) {
+        throw new Error("El archivo aún no existe")
+    }
+
+    const archivo = fs.readFileSync(rutaArchivo, {encoding: "utf-8"})
+
+    const archivoParseado = JSON.parse(file)
+
+    archivoParseado.forEach(user => {
+        console.log(user)
+    })
+}
+
+
+function copiarArchivo(nombreArchivo) {
+    if (!fs.existsSync(nombreArchivo)) {
+        throw new Error("El archivo aún no existe")
+    }
+    const nuevoNombreArchivo = "newAgenda.json"
+
+    if (fs.existsSync(nuevoNombreArchivo)) {
+        throw new Error("El archivo ya existe")
+    }
+
+    const base = process.cwd()
+    const rutaArchivo = path.join(base, nombreArchivo)
+
+
+    fs.copyFileSync(rutaArchivo, nuevoNombreArchivo)
+    console.log("Archivo copiado correctamente")
+
+}
+
+
+function busquedaPorLlave(llave) {
+    if (typeof llave !== 'string') {
+        throw new Error("No puedes introducir un valor numérico como clave")
+    }
+    const usuario = personas.find(persona => persona.dni === llave)
+    if (usuario.length !== 0) {
+        console.log("¡Usuario encontrado!", user)
+        return
+    }
+
+    console.log("El usuario no existe")
+    return
+}
+
+
+
+
+
+function crearArchivoDeStock(numeroCodigo, nivelMinimo, nivelActual, proveedor, precio){
+    const nombreArchivo = "stock.json"
+
+    if (!fs.existsSync(nombreArchivo)) {
+        console.log("El archivo no existe")
+        fs.writeFileSync(nombreArchivo, JSON.stringify([]))
+    }
+
+    
+
+    const archivoParseado = JSON.parse(fs.readFileSync(nombreArchivo, {encoding: "utf-8"}))
+    console.log(archivoParseado)
+    const existeElRegistro = archivoParseado.find(reg => reg.numeroCodigo === numeroCodigo && reg.nivelMinimo === nivelMinimo && reg.nivelActual === nivelActual && reg.proveedor === proveedor && reg.precio === precio)
+
+    if (existeElRegistro) {
+        console.log("Este artículo ya está añadido")
+    }
+
+    const producto = {
+        numeroCodigo,
+        nivelMinimo,
+        nivelActual,
+        proveedor,
+        precio
+    }
+
+
+    archivoParseado.push(producto)
+
+    fs.writeFileSync(nombreArchivo, JSON.stringify(archivoParseado, null, 2))
+
+    console.log("archivo de stock creado correctamente")
+}
+
+
+
+function leerStock(nombreArchivo){
+
+    if (!fs.existsSync(nombreArchivo)) {
+        throw new Error("El archivo no existe")
+    }
+
+    const archivo = fs.readFileSync(nombreArchivo, {encoding: "utf-8"})
+
+    const archivoParseado = JSON.parse(archivo)
+
+    return archivoParseado
+} 
+
+
+function detectarBajoMinimo(nombreArchivo) {
+    const archivoParseado = leerStock(nombreArchivo)
+    const productosDebajoDelMinimo = archivoParseado.filter(prod => prod.nivelMinimo > prod.nivelActual)
+    productosDebajoDelMinimo.forEach(prod => {
+        console.log(`Los productos debajo del mínimo son:  ${prod.proveedor}, ${prod.precio}, NUM_CÓDIGO=${prod.numeroCodigo}`)
+    })
+}
+
+
+function generarPedido(numeroCodigo) {
+    if (!numeroCodigo) {
+        throw new Error("Especifica el código del producto que deseas comprar")
+    }
+    if (typeof numeroCodigo !== 'number') {
+        throw new Error("El código debe ser un valor numérico")
+    }
+    const base = process.cwd()
+    const nombreArchivo = "stock.json"
+    const filePath = path.join(base, nombreArchivo)
+
+    const archivo = fs.readFileSync(filePath, {encoding:"utf-8"})
+    const archivoParseado = JSON.parse(archivo)
+    let prod = archivoParseado.find(p => p.numeroCodigo === numeroCodigo)
+    
+    if (prod) {
+        const nuevoArchivo = [...archivoParseado]
+        console.log(nuevoArchivo)
+        prod.nivelActual++
+
+        nuevoArchivo.push(prod)
+        fs.writeFileSync(filePath, JSON.stringify(nuevoArchivo, null, 2))
+        console.log("Compra realizada correctamente", prod)
+    } else {
+        console.log("No existe el producto, no se pudo realizar la compra")
+        return
+    }
+}
+
+
+
+function reporteFinal(nombreArchivo, numeroCodigo) {
+    const productos = leerStock(nombreArchivo)
+    console.log(`Los productos disponibles son ${JSON.stringify(productos, null, 2)}`)
+
+    detectarBajoMinimo(nombreArchivo)
+
+    generarPedido(numeroCodigo)
+}
+
+
+reporteFinal("stock.json", 102)
+    
+
+
 
 
 
